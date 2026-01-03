bfbe948f76135c41800f0aa57f2ed5d13c6705a7
EyshilaIvanha <eyshilaivanha@gmail.com>
2026-01-03 18:14:25 -0300

    Update Clock.java

--- Alarm/Clock.java
+++ Alarm/Clock.java
@@ -1,35 +1,65 @@
+import javax.sound.sampled.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
-public class Clock {
+public class AlarmClock implements Runnable{
 
-    public static void main(String[] args) {
+    private final LocalTime alarmTime;
+    private final String filePath;
+    private final Scanner scanner;
 
-        // JAVA ALARM CLOCK
+    AlarmClock(LocalTime alarmTime, String filePath, Scanner scanner){
+        this.alarmTime = alarmTime;
+        this.filePath = filePath;
+        this.scanner = scanner;
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalTime alarmTime = null;
+    @Override
+    public void run(){
 
-        String filePath = "C:\\Windows\\Media\\Alarm01.wav";
-
-        while (alarmTime == null) {
+        while(LocalTime.now().isBefore(alarmTime)){
             try {
-                System.out.print("Enter an alarm time (HH:MM:SS): ");
-                String inputTime = scanner.nextLine();
+                Thread.sleep(1000);
 
-                alarmTime = LocalTime.parse(inputTime, formatter);
-                System.out.println("Alarm set for " + alarmTime);
+                LocalTime now = LocalTime.now();
 
-            } catch (DateTimeParseException e) {
-                System.out.println("Invalid format. Please use HH:MM:SS");
+                System.out.printf("\r%02d:%02d:%02d",
+                        now.getHour(),
+                        now.getMinute(),
+                        now.getSecond());
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
             }
         }
 
-        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
-        Thread alarmThread = new Thread(alarmClock);
-        alarmThread.start();
+        System.out.println("\n*ALARM NOISES*");
+        playSound(filePath);
+    }
+    private void playSound(String filePath){
+
+        File audioFile = new File(filePath);
+
+        try(AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile)){
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+            System.out.print("Press Enter to stop the alarm: ");
+            scanner.nextLine();
+            clip.stop();
+            scanner.close();
+        }
+        catch(UnsupportedAudioFileException e){
+            System.out.println("Audio file format is not supported");
+        }
+        catch(LineUnavailableException e){
+            System.out.println("Audio is unavailable");
+        }
+        catch(IOException e){
+            System.out.println("Error reading audio file");
+        }
     }
 }
